e170cd15e2a09524e579bc9c414dd36329f2b0f8
Mfgit31 <Mfreitasnyc@gmail.com>
2022-10-12 21:57:10 -0400

    commit tree 7185b17654a7c48cc4db852de0a18a90a86c1e7d

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUp) {
+    const hqBlock = 42;
+    let blockDifference = pickUp - hqBlock;
+    let numberOfBlocks = Math.abs(blockDifference);
+    return numberOfBlocks;
+};
+
+function distanceFromHqInFeet(blocksToFeet) {
+    let blockDifferenceResult = distanceFromHqInBlocks(blocksToFeet);
+    let totalFeet = blockDifferenceResult * 264;
+    return totalFeet;
+    };
+
+function distanceTravelledInFeet(start, destination) {
+    let absoluteDistance = Math.abs(destination - start) * 264;
+    return absoluteDistance;
+    };
+
+function calculatesFarePrice(start, destination) {
+    let totalFeet = distanceTravelledInFeet(start, destination);
+    
+    if (totalFeet <= 400) return 0;
+    if (totalFeet > 400 && totalFeet <= 2000) return (totalFeet-400)*0.02;
+    if (totalFeet > 2000 && totalFeet < 2500) return 25;
+    return 'cannot travel that far';
+    };
+
+console.log(calculatesFarePrice(1, 10));
